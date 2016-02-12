31130cc97bdf1f214257670f6cdbf220262946b0
Carlos <carlosjesus94@gmail.com>
2016-02-12 12:15:38 -0430

    Trying to make the animation more generic. Use hide/show instead of promises

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 (function($){
-	function carlosAnimation() {
+	function textAnimation(message) {
+		var deferred = $.Deferred();
 		var carret = $(".carret");
+		var animationText = $("#animationText");
+		var carret = $("<span class='carret'>&nbsp;</span>");
+		animationText.html(message);
+		animationText.append(carret);
+		console.log(message);
 
 		setTimeout(function() {
-			carret.addClass("type");
+			if(!carret.hasClass("typing"))
+				carret.addClass("typing");
 		}, 2000);
 
 		carret.on('transitionend', function(e){
 			setTimeout(function() {
-				carret.removeClass("type");
-				carret.addClass("untyping");
+				if (carret.hasClass("typing")){
+					carret.removeClass("typing");
+					carret.addClass("untyping");
+				} else if (carret.hasClass("untyping")) {
+					carret.removeClass("untyping");
+
+					deferred.resolve();
+				}
+				console.log("transitionend");
 			}, 2000);
 		});
+
+		return deferred.promise();
 	}
 
 	$(document).ready(function(){
-		carlosAnimation();
+		textAnimation("Hi, I'm Carlos.")
+		.then(textAnimation("And this is sparta!"));
 	});
 })(jQuery)
